89e8af4fde45219597e210ca32f7b696d721de3e
shihab-shamim <shihabshamim767@gmail.com>
2025-04-16 12:05:20 +0600

    rotate 180 deg complete

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor'), 'version' => '5b1483301d950dc70672');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor'), 'version' => 'c33a4cdd8d0c1182da86');
