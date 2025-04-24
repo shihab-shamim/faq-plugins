aa7dc50575e86c4907e8db77dfd11cf985c55f28
shihab-shamim <shihabshamim767@gmail.com>
2025-04-24 09:44:56 +0600

    all okey the buttons

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor'), 'version' => 'd3066e8b082c5b091f0b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor'), 'version' => '5dbf9002f71240004481');
